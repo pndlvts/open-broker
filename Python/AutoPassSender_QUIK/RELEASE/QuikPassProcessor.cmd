fc3722f9f162043d29e5bdddc27b1d656953fa81
Pandelov Timofey <pndlvts@gmail.com>
2024-02-03 21:20:26 +0300

    refactor:наполнен каталог Release

---
+++ Python/AutoPassSender_QUIK/RELEASE/QuikPassProcessor.cmd
@@ -0,0 +1,4 @@
+REM  echo on
+date /T > log.txt & :: „ β  § ―γαª 
+C:\work\jdk-11.0.2\bin\java.exe -jar C:\Users\pandelov_ts\Desktop\help\3_soft\Quik\mass-letter-sender.jar --componentName=QuikPassProcessor --components.quikPassProcessor.inputCsvPath=C:\Users\pandelov_ts\Desktop\help\3_soft\Quik\In\quik.csv --components.quikPassProcessor.passCsvPath=C:\Users\pandelov_ts\Desktop\help\3_soft\Quik\Temp\mildle.csv >> log.txt
+type log.txt & :: —¨β ¥¬ «®£
