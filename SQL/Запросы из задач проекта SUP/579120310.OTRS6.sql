3811e63bbf549269ae1b18ff4fd2ae336213a7ee
Pandelov Timofey <pndlvts@gmail.com>
2024-12-17 01:32:43 +0300

    otrs

--- SQL/Запросы из задач проекта SUP/579120310.OTRS6.sql
+++ SQL/Запросы из задач проекта SUP/579120310.OTRS6.sql
@@ -1,11 +1,13 @@
 USE opendb
-IF OBJECT_ID('tempdb..##temp_otrs_task') IS NOT NULL
+--IF OBJECT_ID('tempdb..##temp_otrs_task') IS NOT NULL
+IF EXISTS (SELECT 1 FROM ##temp_otrs_task) --работает быстре
     BEGIN
         SELECT  code AS [Код депонента],
                 client_name AS [ФИО],
                 Phone AS [Телефон],
                 phone_cell AS [Мобильный],
                 E_Mail_To AS [E-Mail]
-        FROM client
-        WHERE code IN (SELECT code FROM ##temp_otrs_task)
+        FROM client c
+        --WHERE code IN (SELECT code FROM ##temp_otrs_task)
+        INNER JOIN ##temp_otrs_task t ON c.code = t.code --работает быстрее
     END
